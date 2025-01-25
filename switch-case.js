4331ebbd31e1c6c7df110e342ac741e97a87ff56
Maria Fernanda Urd <MariaFernandaUrd>
2025-01-25 09:33:12 -0600

    se crea ejercicio switch y case

---
+++ switch-case.js
@@ -0,0 +1,13 @@
+let fruta = "manzana";
+console.log("Fruta "+fruta)
+
+switch(fruta){
+case "manzana":
+    console.log("El color de la fruta es amarilla o verde")
+    break;
+case "fresa":
+    console.log("El color de la fruta es rojo o naranjado")
+    break;
+default:
+    console.log("Fruta no  encontrada")
+}
