87a5696aceaae4807ac8c38e8894ae0468ced89c
Maria Fernanda Urd <MariaFernandaUrd>
2025-03-01 13:45:48 -0600

    se agrega tarea de switch y case

---
+++ TareasPlataforma/EjercicioSwitchCase.js
@@ -0,0 +1,283 @@
+//Ejercicio 1
+
+console.log("****Ejercicio 1****")
+let dia = "Lunes";
+
+switch (dia){
+
+    case "Domingo": 
+        console.log("Día de la semana num 1");
+        break;
+    
+    case "Lunes":
+        console.log("Día de la semana num 2");
+        break;
+
+    case "Martes":
+        console.log("Día de la semana numero 3");
+        break;
+    case "Miercoles":
+        console.log("Dia de la semana numero 4");
+        break;
+    case "Jueves":
+        console.log("Dia de la semana numero 5");
+        break;
+    case "Viernes":
+        console.log("Dia de la semana numero 6");
+        break;
+    case "Sabado":
+        console.log("Dia de la semana numero 7");
+        break;
+}
+
+
+//Ejercicio 2
+console.log("****Ejercicio 2****")
+
+let calif = "A"
+switch(calif){
+
+    case "A":
+        console.log("Excelente");
+        break;
+    
+    case "B":
+        console.log("Bueno");
+        break;
+    case "C":
+        console.log("Regular");
+        break;
+    case "D":
+        console.log("Deficiente");
+        break;
+    case "E":
+        console.log ("Reprobado");
+        break;
+    default: 
+        console.log("Calificación invalida")
+       
+    
+    
+}
+
+//Ejercicio 3
+console.log("****Ejercicio 3****")
+let mes = 3;
+
+switch(mes){
+
+    case 1:
+        console.log("Enero");
+        break;
+    
+    case 2:
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log ("Mayo");
+        break;
+    case 5:
+        console.log ("Junio");
+        break;
+    case 6:
+        console.log ("Julio");
+        break;
+    case 7:
+        console.log ("Agosto");
+        break;
+    case 8:
+        console.log ("Septiembre");
+        break;
+    case 9:
+        console.log ("Septiembre");
+         break;
+    case 10:
+        console.log ("Octubre");
+         break;
+    case 11:
+        console.log ("Noviembre");
+         break;
+    case 12:
+        console.log ("Diciembre");
+         break;    
+    default: 
+        console.log("Número inválido")
+       
+    
+    
+}
+
+//Ejercicio 4
+console.log("****Ejercicio 4****")
+let color = "rojo"
+
+switch(color){
+
+    case "rojo":
+        console.log("Alto");
+        break;
+    
+    case "amarillo":
+        console.log("Precaución");
+        break;
+    case "verde":
+        console.log("Siga");
+        break;
+   
+    default: 
+        console.log("Color inválido")
+       
+    
+    
+}
+
+
+
+
+
+//Ejercicio 5
+console.log("****Ejercicio 5****")
+let lados = 2;
+
+switch (lados){
+
+    case 0: 
+        console.log("Triangulo escaleno");
+        break;
+    
+    case 2:
+        console.log("Triángulo isóceles");
+        break;
+
+    case 3:
+        console.log("Triángulo equilátero");
+        break;
+    default: 
+        console.log("Número inválido")
+       
+}
+
+//Ejercicio 6
+console.log("****Ejercicio 6****")
+let estacion = 2;
+
+switch (estacion){
+
+    case 1: 
+        console.log("Primavera");
+        break;
+    
+    case 2:
+        console.log("Verano");
+        break;
+
+    case 3:
+        console.log("Otoño");
+        break;
+    case 3:
+        console.log("Invierno");
+        break;
+    default: 
+        console.log("Número inválido")
+       
+}
+
+//Ejercicio 7
+console.log("****Ejercicio 7****")
+let tipoV = "coche";
+
+switch (tipoV){
+
+    case "coche": 
+        console.log("Primavera");
+        break;
+    
+    case "moto":
+        console.log("Verano");
+        break;
+
+    case "bicicleta":
+        console.log("Otoño");
+        break;
+    case 3:
+        console.log("Invierno");
+        break;
+    default: 
+        console.log("Número inválido")
+       
+}
+
+//Ejercicio 8
+console.log("****Ejercicio 8****")
+let nivU = "admin";
+
+switch (nivU){
+
+    case "admin": 
+        console.log("Todos los permisos");
+        break;
+    
+    case "editor":
+        console.log("Permisos de edición");
+        break;
+
+    case "visitante":
+        console.log("Permisos de lectura");
+        break;
+
+    default: 
+        console.log("Nivel de usuario inválido")
+       
+}
+
+//Ejercicio 9
+console.log("****Ejercicio 9****")
+let fruta = "manzana";
+
+switch (fruta){
+
+    case "manzana": 
+        console.log("Color de la manzana: Roja");
+        break;
+    
+    case "plátano":
+        console.log("Color del plátano: amarillo");
+        break;
+
+    case "naranja":
+        console.log("Color de la naranja: naranja");
+        break;
+
+    default: 
+        console.log("Nivel de usuario inválido")
+       
+}
+
+//Ejercicio 10
+console.log("****Ejercicio 9****")
+let clima = "soleado";
+
+switch (clima){
+
+    case "soleado": 
+        console.log("Recomendación para día soleado: Usa protector solar");
+        break;
+    
+    case "lluvioso":
+        console.log("Recomendación para día lluvioso: Lleva un paraguas");
+        break;
+
+    case "nublado":
+        console.log("Recomendación para día nubleado: Puede que necesites una chamarra");
+        break;
+
+    default: 
+        console.log("Clima inválido")
+       
+}
